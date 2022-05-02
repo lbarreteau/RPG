26c2e7f0910bac6ed4d0a3780429c8055843c87a
MathisCaugant <mathis.caugant@epitech.eu>
2022-05-02 07:22:27 -0400

    [MODIFY] -settings_screen - fix link issue with settings screen

--- sources/screens/settings_screen/settings_screen.c
+++ sources/screens/settings_screen/settings_screen.c
@@ -12,9 +12,7 @@ void settings_screen(screens *screen)
     struct settings setting;
     bool exit = false;
 
-    init_screen_struct(screen);
     init_settings_struct(&setting);
-    set_screen(screen);
     set_settings(&setting);
     while (sfRenderWindow_isOpen(screen->window)) {
         while (sfRenderWindow_pollEvent(screen->window, &screen->event)) {
